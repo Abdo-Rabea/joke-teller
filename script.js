4576f25ce8766f01592fca7e2617a49217f7fec5
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2025-07-01 15:53:23 +0300

    speach api + joke api - all working

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const audioElement = document.getElementById("audio");
+const jokeBtn = document.getElementById("joke-button");
+
 // VoiceRSS Javascript SDK
 const VoiceRSS = {
   speech: function (e) {
@@ -101,3 +104,46 @@ const VoiceRSS = {
     throw "The browser does not support HTTP request";
   },
 };
+
+const joke = "hello world my name is dump ass";
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    // Normally, don't write out API Keys like this, but an exception made here because it's free.
+    key: "e985f868e96c46d9b0789c3855350152",
+    src: jokeString,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+function startloadingJoke() {
+  jokeBtn.disabled = true;
+}
+
+async function getJoke() {
+  try {
+    startloadingJoke();
+    const res = await fetch(
+      "https://v2.jokeapi.dev/joke/Programming?type=single"
+    );
+    const joke = await res.json();
+    // finishLoadingJoke();
+    // calling speach api to speach joke
+    tellMe(joke.joke);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// event listners
+audioElement.onplay = () => {
+  jokeBtn.disabled = true;
+};
+audioElement.onended = () => {
+  jokeBtn.disabled = false;
+};
+jokeBtn.onclick = getJoke;
